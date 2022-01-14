2fe425c29a490cefeba77fedd702c8d24530e930
AgnieszkaJekot <jekot.agnieszka@gmail.com>
2022-01-14 15:23:54 +0000

    including username

--- Read data from database.R
+++ Read data from database.R
@@ -1,5 +1,5 @@
 library(ssh)
 library(data.table)
 
-session <- ssh_connect("trevor.olioex.com:2222", passwd = read.csv("C:\\Users\\jekot\\OLIO Dropbox\\Agnieszka Jekot\\PC\\Documents\\Password.csv")[1,]
+session <- ssh_connect("ivar@trevor.olioex.com:2222", passwd = read.csv("C:\\Users\\jekot\\OLIO Dropbox\\Agnieszka Jekot\\PC\\Documents\\Password.csv")[1,]
                       ,keyfile = "C:\\Users\\jekot\\OLIO Dropbox\\Agnieszka Jekot\\PC\\Documents\\Private Key 2")
